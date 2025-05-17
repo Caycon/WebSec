5aee63f85cbbcf95bc41461ad25b8f149d4c84fd
DangTrieeu <dangtrieu.lb@gmail.com>
2025-05-17 19:14:01 +0700

    db connection

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const subscriptionJob = require("../../services/subscriptionJob");
 async function connect() {
   try {
-    await mongoose.connect("mongodb+srv://nbctan4:ioDTaEk7nTdlptlV@cluster0.dkucpgv.mongodb.net/rises");
+    await mongoose.connect("mongodb+srv://dangtrieulb:1fumuZFs66bCLamK@cluster0.rlvft53.mongodb.net/rises");
     console.log("Connect Successfully!!!");
     subscriptionJob;
     console.log("Subscription job started successfully!");
   } catch (error) {
-    console.log("Connect fail!!!");
-  }
+    console.log("Connect fail!!!", error);
+}
 }
 module.exports = { connect };
